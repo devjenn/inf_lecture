9e0a27fbe00192248de273c2f02f8e7fb1961ed1
lje <zenxdevx@gmail.com>
2021-09-13 00:29:54 +0900

    mvc parameter 실습 및 파일 생성

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -12,5 +13,17 @@ public class HelloController {
 		model.addAttribute("data", "hello!!!");
 		return "hello";
 	}
+	
+	
+	//위에서는 "hello!!"를 직접 받았다면,
+	//이번에는 외부(웹)에서 파라메터를 받을 것임
+	//model은 넘겨줘야 함(model을 잡으면 뷰에서 렌더링할 때 씀)
+	
+	@GetMapping("hello-mvc")
+	public String helloMvc(@RequestParam("name") String name, Model model) {
+		model.addAttribute("name", name); //파라메터로 넘어온 name을 넘겨줌
+		return "hello-template";
+	}
+	
 
 }
